ef91a4e76a1932baa0cc5e8f77bc1e8f872afa11
abizmo <abizmo@gmail.com>
2022-06-23 13:01:51 +0100

    build: Prepare skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,30 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <>
+      {/* <!-- Rating state start --> */}
+      How did we do? Please let us know how we did with your support request.
+      All feedback is appreciated to help us improve our offering! 1 2 3 4 5
+      Submit
+      {/* <!-- Rating state end --> */}
+      {/* <!-- Thank you state start --> */}
+      You selected (Add rating here) out of 5 Thank you! We appreciate you
+      taking the time to give a rating. If you ever need more support, don’t
+      hesitate to get in touch!
+      {/* <!-- Thank you state end --> */}
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
+      ;
+    </>
+  );
 }
 
-export default App
+export default App;
