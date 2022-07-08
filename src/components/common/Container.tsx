43e1f1fcde91ca5fcf3ca6a36fee3034f8bc1d13
Abián Izquierdo <abizmo@gmail.com>
2022-07-08 07:41:59 +0100

    Remove place-self-center from Container

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Container({ children }: { children: React.ReactNode }) {
   return (
     <div
-      className="container place-self-center flex flex-col
+      className="container flex flex-col
         gap-6 mx-auto px-8 py-10 bg-rating-800 text-rating-100
         rounded-2xl max-w-sm"
     >
